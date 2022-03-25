8297e5558eb6beedc3019df628b28ba1af3196c6
AngelCruzO <vce08201@innovaccion.mx>
2022-03-25 17:26:38 -0600

    Delete deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Pasteleria-ViaLactea-VueJS/'
-    : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
